58cd7f31e282bb1cbf9949d8c4695bfc6c6baa02
Christian Montoya <christian.montoya@consensys.net>
2024-05-03 09:59:58 -0700

    dapp

--- packages/snap/README.md
+++ packages/snap/README.md
@@ -2,4 +2,6 @@
 
 A simple Snap that checks if an address has a Farcaster account and displays information about that account in the MetaMask transaction flow. 
 
+It also offers a search feature in the home page where you can search to see if an Ethereum address has a Farcaster account.
+
 All of the code can be found at: https://github.com/Montoya/farcaster-insights
--- packages/snap/src/index.ts
+++ packages/snap/src/index.ts
@@ -1,5 +1,5 @@
-import type { OnTransactionHandler } from '@metamask/snaps-sdk';
-import { panel, heading, text, row } from '@metamask/snaps-sdk';
+import type { OnTransactionHandler, OnHomePageHandler, OnUserInputHandler } from '@metamask/snaps-sdk';
+import { panel, heading, text, row, input, button } from '@metamask/snaps-sdk';
 
 // Handle outgoing transactions.
 export const onTransaction: OnTransactionHandler = async ({ transaction }) => {
@@ -40,3 +40,85 @@ export const onTransaction: OnTransactionHandler = async ({ transaction }) => {
     return null; 
   }
 };
+
+export const onHomePage: OnHomePageHandler = async () => { 
+  const interfaceId = await snap.request({
+    method: "snap_createInterface",
+    params: {
+      ui: panel([
+        input({ 
+          name: 'input', 
+          label: 'Enter an Ethereum address:'
+        }),
+        button({ 
+          value: 'Search', 
+          buttonType: 'button',
+          name: 'search'
+        }),
+      ])
+    },
+  });
+  return { 
+    id: interfaceId
+  }
+}; 
+
+export const onUserInput: OnUserInputHandler = async ({id, event}) => { 
+  if(event.name !== 'search') return; 
+
+  // get address input 
+  const state = await snap.request({
+    method: "snap_getInterfaceState",
+    params: {
+      id,
+    },
+  });
+  const addressInput = state['input']; 
+  if(!addressInput) return; 
+
+  const address = `${addressInput}`.toLowerCase(); 
+
+  // assume a valid address 
+  const response = await fetch(`https://homerow.club/fc/name.php?address=${address}`); 
+  const data = await response.json(); 
+
+  const components = [
+    input({ 
+      name: 'input', 
+      label: 'Enter an Ethereum address:',
+      value: `${address}`
+    }),
+    button({ 
+      value: 'Search', 
+      buttonType: 'button',
+      name: 'search'
+    }),
+  ];
+
+  console.log(data); 
+
+  if(data[`${address}`]) { 
+    const user = data[`${address}`][0]; 
+    components.push(text("This is a known Farcaster account:")); 
+    if(user.custody_address===address) { 
+      components.push(row("Address Type",text("Custody Address"))); 
+    }
+    else { 
+      components.push(row("Address Type",text("Verified Address"))); 
+    }
+    components.push(row("Username", text(`[${user.username}](https://warpcast.com/${user.username})`))); 
+    components.push(row("Display Name", text(user.display_name))); 
+    components.push(row("Follower Count",text(''+user.follower_count))); 
+  }
+  else { 
+    components.push(text("This is not a known Farcaster account.")); 
+  }
+
+  snap.request({
+    method: "snap_updateInterface", 
+    params: { 
+      id,
+      ui: panel(components),
+    },
+  }); 
+}
